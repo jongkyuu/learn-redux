cdfb96c84319d0770ef52e6a24e20e05aac381f3
minerva421 <minerva421.ai@gmail.com>
2023-11-16 03:40:45 +0900

    리액트 프로젝트에 redux 적용

--- src/index.js
+++ src/index.js
@@ -3,12 +3,18 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import "./exercise";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import rootReducer from "./modules";
+
+const store = createStore(rootReducer);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
-        <App />
+        <Provider store={store}>
+            <App />
+        </Provider>
     </React.StrictMode>
 );
 
