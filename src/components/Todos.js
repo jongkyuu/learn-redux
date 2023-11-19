d7e6b428cbb93aa4ad843331caa85ccdd118c4dd
minerva421 <minerva421.ai@gmail.com>
2023-11-20 00:27:28 +0900

    할일 목록 강의내용으로 수정

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import styled, { css } from "styled-components";
 
+// 프리젠테이셔널 컴포넌트
 const TodoItemWrapper = styled.li`
     ${(props) =>
         props.done &&
@@ -29,24 +30,31 @@ function TodoList({ todos, onToggle }) {
 
 function Todos({ todos, onToggle, onCreate }) {
     const [text, setText] = useState("");
-    const onClick = (text) => {
+    // const onClick = (text) => {
+    //     onCreate(text);
+    //     setText("");
+    // };
+
+    const onSubmit = (e) => {
+        e.preventDefault();
         onCreate(text);
         setText("");
     };
 
-    const onChange = (e) => {
-        setText(e.target.value);
-    };
+    const onChange = (e) => setText(e.target.value);
 
     return (
         <div>
-            <input
-                type="text"
-                value={text}
-                onChange={onChange}
-                placeholder="할 일 입력 "
-            ></input>
-            <button onClick={() => onClick(text)}>추가</button>
+            <form onSubmit={onSubmit}>
+                <input
+                    value={text}
+                    onChange={onChange}
+                    placeholder="할 일 입력"
+                />
+                <button type="submit">등록</button>
+                {/* <button onClick={() => onClick(text)}>추가</button> */}
+            </form>
+
             <hr />
             <TodoList todos={todos} onToggle={onToggle} />
         </div>
